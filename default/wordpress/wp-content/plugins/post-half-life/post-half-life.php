e34381148ec0c36fb7cd6fdf00abca42ae3702cd
Klaus Breyer <kb@buddybrand.de>
2014-06-30 13:01:05 +0200

    meta ding

--- default/wordpress/wp-content/plugins/post-half-life/post-half-life.php
+++ default/wordpress/wp-content/plugins/post-half-life/post-half-life.php
@@ -14,13 +14,17 @@ Author URI: http://klaus-breyer.de
 $phl_db_version = "0.1";
 $phl_table_name = $wpdb->prefix . "posthalflife"; 
 function phl_counter() {
-	echo "<!-- plugin counter -->";
+	echo "<!-- plugin counter \n";
+	wp_reset_postdata();
 	if (is_single()) {
+		echo get_the_ID();
+		echo "\n";
+		echo the_ID();
 	    global $wpdb, $phl_table_name;
 		$wpdb->insert( $phl_table_name, array("fetch_date" => current_time("mysql"), "post_id" => get_the_ID(), "referer" => $_SERVER['HTTP_REFERER']));
 	}
 
-	echo "<!-- plugin counter -->";
+	echo "\nplugin counter -->";
 }
 add_action( 'wp_footer', 'phl_counter' );
 
